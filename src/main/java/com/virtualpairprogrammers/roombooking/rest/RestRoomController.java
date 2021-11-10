10e89c6e6384f61143f1a5b9417cb793825a6ad7
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-11-10 14:32:40 +0800

    implements Rest room controller CRUD

---
+++ src/main/java/com/virtualpairprogrammers/roombooking/rest/RestRoomController.java
@@ -0,0 +1,41 @@
+package com.virtualpairprogrammers.roombooking.rest;
+
+import com.virtualpairprogrammers.roombooking.data.RoomRepository;
+import com.virtualpairprogrammers.roombooking.model.entities.Room;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/rooms")
+@AllArgsConstructor
+public class RestRoomController {
+
+    private final RoomRepository roomRepository;
+
+    @GetMapping
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Room getRoom(@PathVariable("id") Long id) {
+        return roomRepository.findById(id).get();
+    }
+
+    @PostMapping()
+    public Room newRoom(@RequestBody Room room) {
+        return roomRepository.save(room);
+    }
+
+    @PutMapping()
+    public Room updateRoom(@RequestBody Room updateRoom) {
+        Room originalRoom = roomRepository.findById(updateRoom.getId()).get();
+        originalRoom.setName(updateRoom.getName());
+        originalRoom.setLocation(updateRoom.getLocation());
+        originalRoom.setCapacities(updateRoom.getCapacities());
+
+        return roomRepository.save(originalRoom);
+    }
+}
